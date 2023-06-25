7219a393df36ba9434d50b2f460445d8f4840c09
Saibal Talukder <akashts724@gmail.com>
2023-06-25 10:41:46 +0600

    working on project

--- app/Views/blog.php
+++ app/Views/blog.php
@@ -47,31 +47,6 @@
                     <?php }
                     
                     ?>
-
-                    <!-- <div class="card" style="width: 20rem; margin:10px">
-                        <img class="card-img-top" src="<?= base_url('images/bangla.png') ?>" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                    <div class="card" style="width: 20rem; margin:10px">
-                        <img class="card-img-top" src="<?= base_url('images/bangla.png') ?>" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                    <div class="card" style="width: 20rem; margin:10px">
-                        <img class="card-img-top" src="<?= base_url('images/bangla.png') ?>" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div> -->
                 </div>
             </div>
 
